4bcc96caf49f47e1f593dfd8e7349cbe4e4ccd09
fglcash <roman2164532@gmail.com>
2025-10-26 13:09:39 +0300

    Добавления проверки на то что мы действительно зашли на вкладку авторизации

---
+++ pages/locators.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN_URL = (By.CSS_SELECTOR, "[href*='login']")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "[name='registration_submit']")
